cde8b95fe619147b9379639ede8c83b88257e9a3
Araba Taylor <araba.taylor20@gmail.com>
2018-02-02 23:30:19 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -2,5 +2,15 @@ CREATE TABLE projects(
   id INTEGER PRIMARY KEY,
   category TEXT,
   funding_goal INTEGER,
-  start_date INTEGER, 
+  start_date INTEGER,
   end_date INTEGER,);
+
+CREATE TABLE users(
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER,);
+
+CREATE TABLE pledges(
+  id INTEGER PRIMARY KEY,
+  user INTEGER,
+  project INTEGER,);
