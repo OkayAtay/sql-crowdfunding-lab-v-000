3250c43aa8e66fb63db1a24cb07679e0c567d833
Araba Taylor <araba.taylor20@gmail.com>
2018-02-03 00:33:39 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -29,6 +29,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
  FROM projects
  LEFT JOIN pledges
  ON projects.id = pledges.project_id
+ GROUP BY projects.title
  WHERE amount_over_goal > 0;"
 end
 
